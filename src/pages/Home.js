2f3e7ae41fc6d0f712a50f1711df3a4fd22f5971
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-06-02 20:45:45 +0300

    React-Hooks-React-Routes-Lab-V6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from '../components/NavBar';
+import MovieCard from '../components/MovieCard';
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/movies')
+      .then(response => response.json())
+      .then(data => setMovies(data))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movies.map(movie => (
+          <MovieCard key={movie.id} id={movie.id} title={movie.title} />
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Home;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,41 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from '../components/NavBar';
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then(response => response.json())
+      .then(data => setMovie(data))
+      .catch(error => setError(error.message));
+  }, [id]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {movie.genres.map(genre => (
+          <span key={genre}>{genre}</span>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
